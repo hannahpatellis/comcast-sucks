ab1a845f49b70c1d4f53744116b57b54406b9d8a
Hannah <hannah@Workforce.local>
2019-02-08 00:40:55 -0500

    Completly remastered using Nightmare. Works, I think

--- index.js
+++ index.js
@@ -1,33 +1,129 @@
-const casper = require('casper').create();
+const Nightmare = require('nightmare');
+const jsonfile = require('jsonfile')
+const moment = require('moment');
 require('dotenv').config();
 
-casper.start();
+let show = true;
 
-casper
-  .then(function(){
-    console.log("--- Process started ---");
-  })
-  .thenOpen("https://www.domain.com/page1")
-  .then(function(){
-    // scrape something
-    this.echo(this.getHTML('h1#foobar'));
-  })
-  .thenClick("#button1")
-  .then(function(){
-    // scrape something else
-    this.echo(this.getHTML('h2#foobar'));
-  })
-  .thenClick("#button2")
-  thenOpen("http://myserver.com", {
-    method: "post",
-    data: {
-        my: 'data',
-    }
-  }, function() {
-      this.echo("data sent back to the server")
-  })
-  .then(function() {
-    console.log("--- Process ended ---");
-  });
+let report;
 
-casper.run(); 
+let $devInfoTbl;
+let $ProcTbl;
+let $eventTbl;
+
+const ts = moment().format('YY-MM-DD HH:mm');
+const file = `./${ts}.json`;
+
+const start = (cb) => {
+  console.log("\n\x1b[41m\x1b[37m", `🚫 COMCAST SUCKS v. 1 for TP-Link TC-7620 🚫`, "\x1b[0m\n");
+  cb();
+}
+
+const getDeviceInfo = () => {
+  const nightmare = Nightmare({ show: show });
+
+  nightmare
+    .goto(`http://${process.env.IP}`)
+    .wait('#login-btn')
+    .type('#userName', process.env.USERNAME)
+    .type('#pcPassword', process.env.PASSWORD)
+    .click('#login-btn')
+    .wait('table#devInfoTbl')
+    .evaluate(() => {
+      return $('table#devInfoTbl').html();
+    })
+    .end()
+    .then(data => {
+      $devInfoTbl = data;
+      console.log("\x1b[47m\x1b[34m", ` SAVE `, "\x1b[0m", "Basic Information saved");
+      getAdvancedInfo();
+    })
+    .catch(err => {
+      closeNote('error', err);
+    });
+}
+
+const getAdvancedInfo = () => {
+  const nightmare = Nightmare({ show: show });
+
+  nightmare
+    .goto(`http://${process.env.IP}`)
+    .wait('#login-btn')
+    .type('#userName', process.env.USERNAME)
+    .type('#pcPassword', process.env.PASSWORD)
+    .click('#login-btn')
+    .wait(3000)
+    .click('#advanced')
+    .wait('table#ProcTbl')
+    .evaluate(() => {
+      return $('table#ProcTbl').html();
+    })
+    .end()
+    .then(data => {
+      $ProcTbl = data;
+      console.log("\x1b[47m\x1b[34m", ` SAVE `, "\x1b[0m", "Advanced Information saved");
+      getLog();
+    })
+    .catch(err => {
+      closeNote('error', err);
+    });
+}
+
+const getLog = () => {
+  const nightmare = Nightmare({ show: show });
+
+  nightmare
+    .goto(`http://${process.env.IP}`)
+    .wait('#login-btn')
+    .type('#userName', process.env.USERNAME)
+    .type('#pcPassword', process.env.PASSWORD)
+    .click('#login-btn')
+    .wait(3000)
+    .click('#advanced')
+    .wait(8000)
+    .click('li.ml2:nth-child(3)')
+    .wait('table#eventTbl')
+    .evaluate(() => {
+      return $('table#eventTbl').html();
+    })
+    .end()
+    .then(data => {
+      $eventTbl = data;
+      console.log("\x1b[47m\x1b[34m", ` SAVE `, "\x1b[0m", "Log saved");
+      fileReport();
+    })
+    .catch(err => {
+      closeNote('error', err);
+    });
+}
+
+const fileReport = () => {
+  console.log("\x1b[47m\x1b[34m", ` ACT  `, "\x1b[0m", "Compiling data");
+  
+  report = {
+    "basic": $devInfoTbl,
+    "advanced": $ProcTbl,
+    "log": $eventTbl
+  }
+
+  jsonfile.writeFile(file, report)
+    .then(() => {
+      console.log("\x1b[47m\x1b[32m", ` DATA `, "\x1b[0m", "Data saved");
+      closeNote('success');
+    })
+    .catch(err => {
+      console.error("\x1b[47m\x1b[31m", ` ERR  `, "\x1b[0m", "Data save error");
+      closeNote('error', err);
+    });
+}
+
+const closeNote = (status, err) => {
+  if(status === 'success') {
+    console.log("\n\x1b[42m\x1b[37m", `   SUCCESS   `, "\x1b[0m\n");
+  } else if(status === 'error') {
+    console.log("\n\x1b[45m\x1b[37m", `    ERROR    `, "\x1b[0m\n");
+    console.error(err);
+  }
+}
+
+start(getDeviceInfo);
